d18d300d8a775038b22a26a7fb96f1de0fcb88a0
Batuhan Şen <batuhan985585@gmail.com>
2023-08-09 16:35:40 +0000

    ethereum rpc enable  8545 - ws 8546

--- init.sh
+++ init.sh
@@ -87,4 +87,5 @@ if [[ $1 == "pending" ]]; then
 fi
 
 # Start the node (remove the --pruning=nothing flag if historical queries are not needed)
+
 ./rapidd start --metrics --pruning=nothing --evm.tracer=json $TRACE --log_level $LOGLEVEL --minimum-gas-prices=0.0001arapid --json-rpc.api eth,txpool,personal,net,debug,web3,miner --api.enable --json-rpc.enable
